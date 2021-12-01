c917eac9ee0e49a8e436b58cdc5253345a851c79
chrishcarter <chrishcarter@gmail.com>
2021-12-01 09:47:12 +0000

    improved message logging

--- d_1/sonarSweep_p1.js
+++ d_1/sonarSweep_p1.js
@@ -11,14 +11,19 @@ function getDataForDay_asArray(day){
 let sonarSweep_data = getDataForDay_asArray(1)
 
 // this assumes array is > 1
-let increaseCounter = 1
+let increaseCounter = 0
 for ( let i = 0 ; i < sonarSweep_data.length ; i++){
     let point_current     = sonarSweep_data[i]
     let point_next        = sonarSweep_data[i+1]
+    let message = "#"+i
     if ( point_current < point_next){
         increaseCounter +=1
-        console.log("#"+i + "\t" + point_current + " < " + point_next +"\tincreaseCounter["+ increaseCounter +"]")
+        message += ( "\t" + point_current + " < " + point_next +"\t[ "+ increaseCounter +" ]")
     }
+    else{
+        message += ("\t" + point_current + " >= " + point_next)
+    }
+    console.log(message)
 }
 
 console.log(sonarSweep_data.length + " measurements\n" + increaseCounter + " are an increase from previous measurement")
