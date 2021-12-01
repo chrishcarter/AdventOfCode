f3a2f24b1298a8e96605c8f4028af95407b0f723
chrishcarter <chrishcarter@gmail.com>
2021-12-01 09:50:57 +0000

    improved message output

--- d_1/sonarSweep_p2.js
+++ d_1/sonarSweep_p2.js
@@ -12,14 +12,20 @@ let sonarSweep_data = getDataForDay_asArray(1)
 
 // this assumes array is > 1
 let increaseCounter = 0
-for ( let i = 0 ; i < sonarSweep_data.length +3 ; i++){
+for ( let i = -3 ; i < sonarSweep_data.length +3 ; i++){
     let sum_previous    = parseInt(sonarSweep_data[i-1])   + parseInt(sonarSweep_data[i])   + parseInt(sonarSweep_data[i+1])
     let sum_current     = parseInt(sonarSweep_data[i])   + parseInt(sonarSweep_data[i+1])   + parseInt(sonarSweep_data[i+2])
     let sum_next        = parseInt(sonarSweep_data[i+1])   + parseInt(sonarSweep_data[i+2])  + parseInt(sonarSweep_data[i+3])
+
+    let message = "#"+i
     if ( sum_current < sum_next){
         increaseCounter +=1
-        console.log("#"+i + "\t" + sum_current + " < " + sum_next +"\tincreaseCounter["+ increaseCounter +"]")
+        message += ( "\t" + sum_current + " < " + sum_next +"\t[ "+ increaseCounter +" ]")
     }
+    else{
+        message += ("\t" + sum_current + " >= " + sum_next)
+    }
+    console.log(message)
 }
 
 console.log(sonarSweep_data.length + " sets can be compared\n" + increaseCounter + " sets are an increase from previous set")
