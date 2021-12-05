92a4d079dcae4218080a2fae2b6bb3038e616ddc
chrishcarter <chrishcarter@gmail.com>
2021-12-05 19:19:53 +0000

    WIP giant squid

--- 2021/d_4/giantSquid_p1.js
+++ 2021/d_4/giantSquid_p1.js
@@ -1,37 +1,66 @@
+const fs        = require("fs")
+const data_test = fs.readFileSync(process.cwd() + "\\d_4\\data_test.txt").toString().split("\n")
+const data      = fs.readFileSync(process.cwd() + "\\d_4\\data.txt").toString().split("\n")
+const boardStart        = 2
+const boardSize         = 5
+//console.log(data_test)
+//console.log(data)
 
+function board(number, valueArray_2d){
+    this.number = number
+    this.valueArray_2d = valueArray_2d
+}
 
-function getDataForDay_asArray(day, is_test){
-    let fs              = require('fs')
-    let filename        = is_test? "data_test.txt" : "data.txt"
-    let fileContent     = fs.readFileSync(process.cwd() + "\\" + "d_" + day.toString() + "\\" + filename).toString()
-    let dataArray       = fileContent.split(/\r?\n/)
-    return dataArray
+function displayBoard(in_board){
+    console.log("about to print board")
+    const numbers = in_board.valueArray_2d
+    let message = ""
+    for(let y = 0 ; y < boardSize ; y++){
+        message += '\n'
+        //console.log()
+        for(var x = 0 ; x < boardSize ; x++){
+            //console.log(x)
+            //console.log(y)
+            //console.log(numbers[x][y])
+            message += numbers[y][x] + ','
+        }
+    }
+    return message
 }
 
 function get_finalScore(dataSet ){
-    const calls = dataSet[0]
+    const calls = dataSet[0].split(',').map(x=> parseInt(x))
     if(is_debug) console.log(calls)
 
-    // make up each of the boards they are all 5*5
+    
+    const numberofBoards    = ( dataSet.length -1) / (boardSize+1)
+    const boards = new Array()
+    console.log(numberofBoards)
+    for(let i =0 ; i < numberofBoards; i++){
+        let selBoard_firstLine  = boardStart + i*(boardSize+1)
+        let selBoard_lastLine   = selBoard_firstLine + boardSize
+        // I NEED TO CONVERT NUMBERS TO INTEGERS HERE
+        const boardNumbers      = dataSet.slice(selBoard_firstLine,selBoard_lastLine)
+        const selBoard          = new board(i,boardNumbers)
+        boards.push(selBoard)
 
-    const boardSize = 5
+        console.log(displayBoard(selBoard))
+    }
+
+    if(is_debug) console.log(boards)
+    // make up each of the boards they are all 5*5
     //starting from line 3
     //      next 5 lines are the array
     //      skip a line
-    if(is_debug) console.log("number of boards = " +( dataSet.length -1) / (boardSize+1) + "\t from dataset of length : " + dataSet.length)
-    //const boards = 
-
-
     
     return 10
 }
 
-let is_debug = true
-
-let test_result         = get_finalScore(getDataForDay_asArray(4, true))
+let is_debug            = true
+let test_result         = get_finalScore(data_test)
 let test_knownAnswer    = 4512
 
 console.assert(test_result == test_knownAnswer , "test_result["+test_result+ "] =/= test_knownAnswer [" + test_knownAnswer + "]")
 
-let finalScore    = get_finalScore(getDataForDay_asArray(4))
-console.log("p1 Answer = " + finalScore)
+//let finalScore          = get_finalScore(data)
+//console.log("p1 Answer = " + finalScore)
